62b907fdac7885d566d962246a0e75bb6fae74db
jprl123 <joaoprl1@al.insper.edu.br>
2021-05-19 16:40:36 -0300

    criando pastas suportes e configurações

---
+++ config.py
@@ -0,0 +1,39 @@
+from os import path
+
+
+
+#Estabelece a pasta que contem as figuras e sons.
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
+
+
+
+# Dados gerais do jogo.
+
+WIDTH = 960 # Largura da tela
+HEIGHT = 1280 # Altura da tela
+FPS = 60 # Frames por segundo
+
+# Define tamanhos
+RATO_WIDTH = 50 #TROCAR VALORES
+RATO_HEIGHT = 38
+QUEIJO_WIDTH = 50
+QUEIJO_HEIGHT = 38
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+
+
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
+
+
