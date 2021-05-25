81202e904f41e8bd8b1dacc02a0369eab4c02542
itsvictorpacheco <victorpachecotrabalhos@gmail.com>
2021-05-25 17:22:55 -0300

    arrumando mapas

--- arrumamapa.py
+++ arrumamapa.py
@@ -1,7 +1,15 @@
-with open('mapa33.txt', 'r') as arquivo:
-    arquivo = arquivo.readlines()
-    for i in arquivo:
-        for j in i:
-            while len(j) < 32*4:
-                j = j.remove(j[-1])
-    
+with open('mapa1.txt', 'r') as arquivo:
+    linhas = arquivo.readlines()
+    f = []
+    for linha in linhas:
+        f.append(linha[:4*32])
+    f = str(f)
+    f = f.replace('[', '')
+    f = f.replace(']', '')
+    f = f.replace("'", '')
+    f = f.replace("\\n", '')
+    f = f.replace(",", '\n')
+    f = f.replace("    ", '')
+    f = str(f)
+with open('mapa11.txt', 'w') as x:
+    x.write(f)
