743be7ccd5ceec9ded2dbb04912fdc027dfbad3a
jprl123 <joaoprl1@al.insper.edu.br>
2021-05-29 16:14:14 -0300

    separando o jogo

--- tela_inicial2.py
+++ tela_inicial2.py
@@ -1,31 +1,37 @@
 import pygame
 from config import *
 from assets import *  
-import os
+
 pygame.init()
 # ----- Gera tela principal e icone de jogo
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('O rato e a rata')
 
-game = True
-start = True
-# ===== Loop principal =====
-while game:
+def init_screen(screen):
     assets = load_assets()
-    while start:
-        # ----- Trata eventos
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
-            # ----- Verifica consequências
+            # Verifica se foi fechado.
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    start == False    
-        screen.fill((0, 0, 0))
+                state = QUIT
+                running = False
+
+            if event.type == pygame.KEYUP:
+                state = GAME
+                running = False
+
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
         screen.blit(assets[MENU], (0,0))
-    # ----- Gera saídas
-    # ----- Atualiza estado do jogo
-        pygame.display.update()
-        
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+    return state
