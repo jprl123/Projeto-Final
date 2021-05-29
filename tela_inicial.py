582d36df3a62886af186870a02184b4472130a17
lucasgurgeln <lucasg3@al.insper.edu.br>
2021-05-29 14:48:05 -0300

    começo da tela inicial

--- tela_inicial.py
+++ tela_inicial.py
@@ -1,6 +1,6 @@
 import pygame
 from config import *
-
+from assets import *  
 pygame.init()
 # ----- Gera tela principal e icone de jogo
 
@@ -11,9 +11,17 @@ gameIcon = pygame.image.load(path.join(img_dir, 'queijo.png'))
 pygame.display.set_icon(gameIcon)
 
 game = True
-
+start = False
 # ===== Loop principal =====
 while game:
+    if start == False:
+        pass
+
+
+
+
+
+
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
@@ -22,5 +30,6 @@ while game:
 
     # ----- Gera saídas
     screen.fill((255, 255, 255))  # Preenche com a cor branca
+    screen.blit(assets[BACKGROUND], (0, 0))
     # ----- Atualiza estado do jogo
     pygame.display.update()
