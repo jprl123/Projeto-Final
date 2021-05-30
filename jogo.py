2cc5af207b7f4a7aa659d7f78482a22d3d14550a
jprl123 <joaoprl1@al.insper.edu.br>
2021-05-29 22:01:53 -0300

    ....

--- jogo.py
+++ jogo.py
@@ -1,32 +1,31 @@
 # ===== Inicialização =====
 # ----- Importa e inicia pacotes
-from os import stat
-from tela_inicial2 import init_screen
-from mapas import *
 import pygame
 from config  import *
-from assets import *
-import random
-from sprites import *
-from game_screen import *
+from tela_inicial2 import init_screen, tutorial_screen
+from game_screen import game_screen
+
+
+
 pygame.init()
 pygame.mixer.init()
 
 # ----- Gera tela principal e icone de jogo
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('O rato e a rata')
+pygame.display.set_caption('O Rato e a Rata')
 
 gameIcon = pygame.image.load(path.join(img_dir, 'queijo.png'))
 pygame.display.set_icon(gameIcon)
 
 state = INICIA
-while state != QUIT:
+while state != SAIR:
     if state == INICIA:
         state == init_screen(screen)
+    elif state == INST:
+        state == tutorial_screen(screen)
     elif state == JOGAR:
         state == game_screen(screen)
     else:
-        state == QUIT
+        state == SAIR
 pygame.quit()                
 
